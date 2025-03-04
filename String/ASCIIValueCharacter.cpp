e8163e1351070fbcd2dd1d4dc4491d7fa56f288a
RajTangadi03 <rtangadi03@gmail.com>
2025-03-04 18:55:16 +0530

    Structure Updated & Problem Solved.

---
+++ String/ASCIIValueCharacter.cpp
@@ -0,0 +1,18 @@
+// Find the ASCII value of a character
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    char c;
+    cin >> c;
+    cout << int(c) << endl;
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
