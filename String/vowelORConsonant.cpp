69f1d0fc2a439e8c5406bf53069e924e88a09eae
RajTangadi03 <rtangadi03@gmail.com>
2025-03-06 23:00:37 +0530

    Problem Solved.

---
+++ String/vowelORConsonant.cpp
@@ -0,0 +1,20 @@
+// Check whether a character is a vowel or consonant
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    char c;
+    cin >> c;
+    unordered_set<char> s = {'a','e','i','o','u','A','E','I','O','U'};
+    if(s.find(c) != s.end())    cout << "vowel" << endl;
+    else    cout << "consonant" << endl;
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
