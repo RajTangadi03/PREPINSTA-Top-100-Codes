86582f1db5471258623d150aa193e3d6ebc5031b
RajTangadi03 <rtangadi03@gmail.com>
2025-03-11 21:26:41 +0530

    Details Updated.

--- Getting Started/primeNumbersInRange.cpp
+++ Getting Started/primeNumbersInRange.cpp
@@ -1,5 +1,9 @@
 // Prime number within a given range
 
+/*  Given a range between Left and Right we have to find all primes in between them.
+    EX. l = 10, r = 20; Ans = 11, 13, 17, 19;
+*/
+
 #include<bits/stdc++.h>
 
 using namespace std;
