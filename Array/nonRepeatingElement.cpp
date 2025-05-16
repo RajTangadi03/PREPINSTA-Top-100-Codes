2dd5442bddf018848db7d4982e50089429f33480
RajTangadi03 <rtangadi03@gmail.com>
2025-05-16 15:16:43 +0530

    Problem Solved.

---
+++ Array/nonRepeatingElement.cpp
@@ -0,0 +1,33 @@
+// Finding Non Repeating elements in an Array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define lli long long int 
+
+void solve()
+{
+    unordered_map<int, bool> umap;
+    int m;
+    cin >> m;
+    while(m--)
+    {
+        int n;
+        cin >> n;
+        if(umap.find(n) == umap.end()) umap[n] = true;
+        else umap[n] = false;
+    }
+    for(auto i : umap)  if(i.second)    cout << i.first << " ";
+    cout << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    lli t;
+    cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
